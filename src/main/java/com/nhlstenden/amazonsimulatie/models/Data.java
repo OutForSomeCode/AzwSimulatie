808e9712386b15077384f61575f8eec439c07c14
Edgar <Edgar@AnotherFoxGuy.com>
2019-10-29 14:07:50 +0100

    :wrench: 20 modules

--- src/main/java/com/nhlstenden/amazonsimulatie/models/Data.java
+++ src/main/java/com/nhlstenden/amazonsimulatie/models/Data.java
@@ -4,7 +4,7 @@ public final class Data {
   public static final String[] cargoType = {"kaas", "donut", "boter", "melk"};
   public static final int cargoSize = 10;
 
-  public static final int modules = 25;
+  public static final int modules = 20;
   public static final int[] rackPositionsX = {3, 4, 7, 8, 11, 12, 15, 16, 19, 20};
 
   public static final int[][] rackSpawnPositions = {
