8b020c53a65cc7c15d095efbbd53f1aa1a15f46a
Edgar <Edgar@AnotherFoxGuy.com>
2019-10-29 14:03:32 +0100

    :bug: Fixed HTTPS

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -14,7 +14,7 @@ module.exports = merge(common, {
     new Webpack.DefinePlugin({
       'WEBSOCKETHOST': JSON.stringify('wss://sim.a-random-vps.cf/connectToSimulation'),
       'process.env.NODE_ENV': JSON.stringify('production'),
-      'HOST': JSON.stringify('http://sim.a-random-vps.cf/')
+      'HOST': JSON.stringify('https://sim.a-random-vps.cf/')
     }),
     new Webpack.optimize.ModuleConcatenationPlugin(),
     /*new MiniCssExtractPlugin({
